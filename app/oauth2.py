644e18a003d115a0bb1ae0d5aee91cf4c4fa0bf5
bkina <boubacarkina1@gmail.com>
2025-10-03 05:06:04 +0100

    get current user

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,10 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
 
 # SECRET KEY
 # Algorithm
@@ -13,7 +17,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -33,4 +37,13 @@ def verify_access_token(token: str, credentials_exception):
         token_data = schemas.TokenData(id=id)
     except JWTError:
         raise credentials_exception
+    
+    return token_data
 
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail=f"Could not validate credentials",
+                                          headers={"WWW-Authentication": "Berear"})
+    
+    return verify_access_token(token, credentials_exception=credentials_exception)
