32ae5a929e891d0d3147aa056f9d07c7274b150c
bkina <boubacarkina1@gmail.com>
2025-10-05 02:14:45 +0100

    config of environment variables

--- app/main.py
+++ app/main.py
@@ -1,41 +1,17 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional, List
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import engine, get_db
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import post, user, auth
+from .config import settings
 
 
 models.Base.metadata.create_all(bind=engine)
 
-
 app = FastAPI()
-
-
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
-                                password='password', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successfull!")
-        break
-    except Exception as error:
-        print("Conection to database failed")
-        print("Error: ", error)
-        time.sleep()
-
-
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
-
 
 @app.get("/")
 def root():
