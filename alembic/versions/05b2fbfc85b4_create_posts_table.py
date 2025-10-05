3c23dfb60b799a1413ac184d0cc2d6e33291664d
bkina <boubacarkina1@gmail.com>
2025-10-05 23:01:02 +0100

    install & config Alembic

---
+++ alembic/versions/05b2fbfc85b4_create_posts_table.py
@@ -0,0 +1,28 @@
+"""create posts table
+
+Revision ID: 05b2fbfc85b4
+Revises: 
+Create Date: 2025-10-05 22:57:14.859331
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '05b2fbfc85b4'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
